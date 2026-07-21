84e7f9faa1c1aee9c36ff4d1533a502251b279ac
Özkan Demircan <77466293+ozkandmrcn@users.noreply.github.com>
2026-07-21 10:40:42 +0300

    fix: refresh installed PWA after OCR update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="life-search-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.webmanifest","icon.svg"]))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+const CACHE="life-search-v2";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html","manifest.webmanifest","icon.svg"])).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
